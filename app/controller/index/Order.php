39cfee20b35eb8c4cebb98bf1dacac433ae8a191
lcl123456 <13586764835@163.com>
2020-06-30 22:37:58 +0800

    vip充值

--- app/controller/index/Order.php
+++ app/controller/index/Order.php
@@ -94,8 +94,65 @@ class Order extends IndexBase
         $group_id = input('group_id',0);
 
         $user = Db::table("user")->where('id', $userId)->find();
-        $goods = Db::table("vip_level")->where('id', $group_id)->find();
-// vip_delay_day
+        $level = Db::table("vip_level")->where('id', $group_id)->find();
+        if (!$level) {
+            return failed_response('非法攻击！');
+        }
+
+
+        if ($user["user_money"] < $level['vip_fee']) {
+            return failed_response('账户余额不足，请先充值！', 100);
+        }
+        $remainMoney =  $user["user_money"] - $level['vip_fee'] ;
+
+        $maxtime = max(time(), $user['vip_deadline']);
+        $newDeadline = $maxtime + 24 * 3600 * $level['vip_delay_day'];
+
+        $curDate = date("Y-m-d H:i:s");
+
+        Db::startTrans();
+
+        try {
+            // 更改金额
+            Db::table("user")
+                ->where('id','=', $userId)
+                ->update([
+                    'user_money'   => $remainMoney,
+                    'vip_deadline' => $newDeadline,
+                    'update_time'  => $curDate,
+                ]);
+
+            // 订单
+//            $orderId = Db::table("order")
+//                ->insertGetId([
+//                    'order_progress' => 1,
+//                    'buy_user_id'    => $userId,
+//                    'order_money'    => $goods["goods_price"],
+//                    'create_time'    => $curDate,
+//                    'unique_code'    => date("ymdHis") . '-' . StringTool::random(6),
+//                ]);
+
+            // 操作交易记录
+            Db::table("transaction_record")
+                ->insertGetId([
+                    'user_id'           => $userId,
+                    'operator_type'     => 1,
+                    'money'             => $level['vip_fee'],
+                    'transaction_type'  => 5,
+                    'order_id'          => 0,
+                    'create_time'       => $curDate,
+                ]);
+
+            Db::commit();
+        } catch (\Exception $e) {
+
+            Db::rollback();
+            return failed_response($e->getMessage());
+        }
+
+
+        return success_response();
+
     }
 
     public function index()
--- app/controller/index/User.php
+++ app/controller/index/User.php
@@ -18,6 +18,34 @@ class User extends IndexBase
         }
 
         $user = Db::table("user")->where('id', session('user_id'))->find();
+        $deadline = $user['vip_deadline'];
+        if ($deadline < time()) {
+            $deadline = 0;
+        } else {
+            $gapDay = ceil(($deadline - time()) / (3600 * 24));
+            if ($gapDay < 31) {
+                $deadline = $gapDay . '天';
+            } else {
+                $gapMonth = ceil($gapDay / 30);
+                $year = floor($gapMonth / 12);
+                $month = $gapMonth % 12;
+
+                $deadlineYear = $deadlineMonth = '';
+                if (!empty($year)) {
+                    $deadlineYear = $year . "年";
+                }
+
+                if ($year < 10) {
+                    if (!empty($month)) {
+                        $deadlineMonth = $month . "个月";
+                    }
+                }
+
+                $deadline = $deadlineYear . $deadlineMonth;
+            }
+        }
+        $user['deadline_day'] = $deadline;
+//        dump($user);
         $vipLevel = Db::table("vip_level")
             ->where('is_use','=',1)
             ->order('sort_order')
